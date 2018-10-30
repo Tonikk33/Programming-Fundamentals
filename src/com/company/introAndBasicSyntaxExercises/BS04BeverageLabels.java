b8d1cc2c729a071a00227552009fb123e284c948
Tonik33 <g.pantov@abv.bg>
2018-10-30 22:09:13 +0200

    Small change of format last task

--- src/com/company/introAndBasicSyntaxExercises/BS04BeverageLabels.java
+++ src/com/company/introAndBasicSyntaxExercises/BS04BeverageLabels.java
@@ -1,26 +1,24 @@
 package com.company.introAndBasicSyntaxExercises;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class BS04BeverageLabels {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String name = scanner.nextLine();
-        double ProductVolume = Double.parseDouble(scanner.nextLine());
-        double ProductEnergy = Double.parseDouble(scanner.nextLine());
-        double ProductSugarContent = Double.parseDouble(scanner.nextLine());
+        DecimalFormat df = new DecimalFormat("#.###############");
 
-        double energy =(ProductVolume * ProductEnergy)/100.00;
-        double sugar =(ProductSugarContent * ProductVolume)/100.00;
+        String productName = scanner.nextLine();
+        int volume = Integer.parseInt(scanner.nextLine());
+        int energyContent = Integer.parseInt(scanner.nextLine());
+        double sugarContent = Double.parseDouble(scanner.nextLine());
 
-        System.out.printf("%.0fml %s:%n", ProductVolume, name);
+        double energyContentInGivenVolume = (volume * energyContent) / 100.0;
+        double sugarContentInGivenVolume = (volume * sugarContent) / 100.0;
 
-        if (sugar%2==0) {
-            System.out.printf("%.0fkcal, %.0fg sugars", energy, sugar);
-        }
-        else {
-            System.out.printf("%.0fkcal, %.1fg sugars", energy, sugar);
-        }
+        System.out.printf("%dml %s:\n%skcal, %sg sugars",
+                volume, productName, df.format(energyContentInGivenVolume),
+                df.format(sugarContentInGivenVolume));
     }
 }
